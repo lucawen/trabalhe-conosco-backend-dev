fbc6ea6c3882ed0c9a7dcd04dbd941b082f71dc1
Luca Albuquerque <lucalacerda1@gmail.com>
2018-01-11 13:33:49 -0200

    Deploy

--- picpay/users/static/js/main.js
+++ picpay/users/static/js/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Buefy from 'buefy'
-import 'buefy/lib/buefy.css'
 import App from './App.vue'
+import 'buefy/lib/buefy.css';
 
 Vue.use(Buefy, {
     defaultIconPack: 'fa'
