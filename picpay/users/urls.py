f5b123c26da554a8935bdfe59117b87700f89595
Luca Albuquerque <lucalacerda1@gmail.com>
2018-01-11 14:01:08 -0200

    user url

--- picpay/users/urls.py
+++ picpay/users/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from users.views import UsersPicpayListView
 
 urlpatterns = [
-    path('list/', UsersPicpayListView.as_view()),
+    path('', UsersPicpayListView.as_view()),
 ]
